d88f613d8b8bcd6bfe2103b67ccae49a7e3f4942
miguelcoria94 <miguelcoria94@gmail.com>
2024-03-10 13:22:44 -0500

    added check winner logic

--- script.js
+++ script.js
@@ -23,40 +23,92 @@ function init() {
 
 function render() {
   renderBoard();
+  renderCurrentPlayerMessage();
   renderWinner();
-  console.log(board);
 }
 
 function renderWinner() {
   if (winner) {
-    winningMessage.innerHTML = winner;
+    winningMessage.innerHTML = `Player ${winner !== 1 ? "O" : "X"} Wins!`;
+    return;
+  }
+  return;
+}
+
+function renderCurrentPlayerMessage() {
+  if (!winner) {
+    currentPlayerMessage.innerHTML = `Player ${turn === 1 ? "O" : "X"}'s turn`;
     return;
   }
   return;
 }
 
 function renderBoard() {
-  count = 0;
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
       const squareIndex = i * 3 + j;
       squares[squareIndex].innerHTML = board[i][j];
     }
   }
-  console.log(board);
 }
 
-// handle a player clicking a square
 squares.forEach(function (el) {
   el.addEventListener("click", function (e) {
+    e.preventDefault()
+    if(winner){
+        return 
+    }
     playTurn(e.target);
   });
 });
 
 function checkForWinner() {
-    // check for a winner
-    // undate winner data
-    // render the game
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      let letter = board[i][j];
+      if (letter != null && checkNeighbors(i, j, letter) >= 2) {
+        winner = turn;
+        return;
+      }
+    }
+  }
+}
+
+function checkNeighbors(
+  row,
+  col,
+  letter,
+  count = 0,
+  neighborDirs = [
+    [-1, -1],
+    [0, -1],
+    [1, -1],
+    [-1, 0],
+    [1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+  ]
+) {
+  if (count >= 2) {
+    return count;
+  }
+  for (let i = 0; i < neighborDirs.length; i++) {
+    let dir = [neighborDirs[i]];
+    let nextRow = row + dir[0][0];
+    let nextCol = col + dir[0][1];
+    if (
+      nextRow >= 0 &&
+      nextRow < board.length &&
+      nextCol >= 0 &&
+      nextCol < board[0].length &&
+      board[nextRow][nextCol] === letter
+    ) {
+      return checkNeighbors(nextRow, nextCol, letter, count + 1, dir);
+    }
+  }
+
+  return 0;
 }
 
 function playTurn(clickedSquare) {
@@ -70,9 +122,10 @@ function playTurn(clickedSquare) {
   } else {
     board[row][col] = "X";
   }
+
   turn *= -1;
+  checkForWinner();
   render();
-  checkForWinner()
 }
 
 // handle a player clicking the replay button
